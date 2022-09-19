bdf7cc1076ca06cd75e9e86544b4a49ebe6b01e2
Maargarita <prosvetova_02@mail.ru>
2022-09-19 15:40:33 +0300

    Add all

---
+++ Fuel-mass-calculator/Form2.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Fuel_mass_calculator
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TextBox_KPD = new System.Windows.Forms.TextBox();
+            this.TextBox_KG = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.TextBox_C = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.ComboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(71, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(383, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Эта программа рассчитывает необходимое колличество";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(105, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(314, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "коксованного угля для расплавления мталлов";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(150, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(224, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "на основании введенных данных";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(23, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(182, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Введите КПД печи (%)";
+            // 
+            // TextBox_KPD
+            // 
+            this.TextBox_KPD.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.TextBox_KPD.Location = new System.Drawing.Point(329, 103);
+            this.TextBox_KPD.Name = "TextBox_KPD";
+            this.TextBox_KPD.Size = new System.Drawing.Size(128, 26);
+            this.TextBox_KPD.TabIndex = 4;
+            // 
+            // TextBox_KG
+            // 
+            this.TextBox_KG.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.TextBox_KG.Location = new System.Drawing.Point(329, 153);
+            this.TextBox_KG.Name = "TextBox_KG";
+            this.TextBox_KG.Size = new System.Drawing.Size(128, 26);
+            this.TextBox_KG.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label5.Location = new System.Drawing.Point(23, 153);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(223, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Введите массу металла (кг)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label6.Location = new System.Drawing.Point(23, 253);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(223, 20);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Выберете металл из списка";
+            // 
+            // TextBox_C
+            // 
+            this.TextBox_C.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.TextBox_C.Location = new System.Drawing.Point(329, 203);
+            this.TextBox_C.Name = "TextBox_C";
+            this.TextBox_C.Size = new System.Drawing.Size(128, 26);
+            this.TextBox_C.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label7.Location = new System.Drawing.Point(23, 203);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(289, 20);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Введите начальную температуру (С)";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.button1.Location = new System.Drawing.Point(27, 315);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 33);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.button2.Location = new System.Drawing.Point(327, 315);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 33);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Закрыть";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ComboBox1
+            // 
+            this.ComboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.ComboBox1.FormattingEnabled = true;
+            this.ComboBox1.Items.AddRange(new object[] {
+            "al",
+            "au ",
+            "ag",
+            "cr",
+            "cu",
+            "cs ",
+            "ca",
+            "co",
+            "k",
+            "li",
+            "mg",
+            "ni",
+            "na",
+            "pt",
+            "pb",
+            "sn",
+            "ti",
+            "w",
+            "zn",
+            "zr"});
+            this.ComboBox1.Location = new System.Drawing.Point(327, 253);
+            this.ComboBox1.Name = "ComboBox1";
+            this.ComboBox1.Size = new System.Drawing.Size(130, 28);
+            this.ComboBox1.TabIndex = 13;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 373);
+            this.Controls.Add(this.ComboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TextBox_C);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.TextBox_KG);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TextBox_KPD);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Рассчет массы топлива";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TextBox_KPD;
+        private System.Windows.Forms.TextBox TextBox_KG;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox TextBox_C;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox ComboBox1;
+    }
+}
---
+++ Fuel-mass-calculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fuel_mass_calculator
+{
+    static class Program
+    {
+        public static ApplicationContext Form0;
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Form0 = new ApplicationContext(new Form1());
+            Application.Run(Form0);
+        }
+
+        public static void OpenForm(Form current, Form next)
+        {
+            Form0.MainForm = next;
+            current.Close();
+            Form0.MainForm.Show();
+        }
+    }
+}
